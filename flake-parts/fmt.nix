38ccc9f4de429aba75cdac005da0b386f152300d
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-03-03 20:50:30 +0700

    chore(flake): bump inputs

--- flake-parts/fmt.nix
+++ flake-parts/fmt.nix
@@ -3,26 +3,31 @@
   partitions.dev.module = devPartition: {
     imports = [ devPartition.inputs.treefmt.flakeModule ];
 
-    perSystem = psArgs: {
-      pre-commit.settings.hooks.treefmt.enable = true;
+    perSystem =
+      psArgs@{ pkgs, ... }:
+      {
+        pre-commit.settings.hooks.treefmt.enable = true;
 
-      treefmt = {
-        projectRootFile = "flake.nix";
-        programs = {
-          nixfmt.enable = true;
-          prettier.enable = true;
-          taplo = {
-            enable = true;
-            settings.formatting = {
-              reorder_keys = true;
-              reorder_arrays = true;
-              reorder_inline_tables = true;
-              allowed_blank_lines = 1;
+        treefmt = {
+          projectRootFile = "flake.nix";
+          programs = {
+            nixfmt = {
+              enable = true;
+              package = pkgs.nixfmt;
+            };
+            prettier.enable = true;
+            taplo = {
+              enable = true;
+              settings.formatting = {
+                reorder_keys = true;
+                reorder_arrays = true;
+                reorder_inline_tables = true;
+                allowed_blank_lines = 1;
+              };
             };
           };
+          settings.on-unmatched = "fatal";
         };
-        settings.on-unmatched = "fatal";
       };
-    };
   };
 }
