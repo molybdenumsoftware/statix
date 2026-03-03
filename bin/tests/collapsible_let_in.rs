9c0f576a2c042b629de4344924efb766fa557ec5
Lymah123 <fimihanodunola@gmail.com>
2026-03-03 22:42:51 +0100

    fix(collapsible_let_in): don't warn when collapsing would cause infinite recursion
    When the inner let redefines a name that appears in the outer let's
    binding values, collapsing the two let-in expressions would cause
    infinite recursion. Add a check to detect this case and skip the lint.

--- bin/tests/collapsible_let_in.rs
+++ bin/tests/collapsible_let_in.rs
@@ -1,6 +1,9 @@
 mod _utils;
+
 use indoc::indoc;
+
 use macros::generate_tests;
+
 generate_tests! {
     rule: collapsible_let_in,
     expressions: [
@@ -9,11 +12,11 @@ generate_tests! {
               a = 2;
               b = 3;
             in
-              let
-                c = 5;
-                d = 6;
-              in
-              a + b + c + d
+            let
+              c = 5;
+              d = 6;
+            in
+            a + b + c + d
         "},
         indoc! {r"
             {
@@ -26,6 +29,42 @@ generate_tests! {
               pkgs = pkgs';
             in
             pkgs
+        "},
+        indoc! {r"
+            let
+              a.b = 1;
+            in
+            let
+              c = 2;
+            in
+            c
+        "},
+        indoc! {r"
+            let
+              inherit pkgs;
+            in
+            let
+              pkgs = pkgs.extend (import ./overlay.nix);
+            in
+            pkgs
+        "},
+        indoc! {r"
+            let
+              inherit (foo) bar;
+            in
+            let
+              bar = 1;
+            in
+            bar
+        "},
+        indoc! {r"
+            let
+              x = a.b;
+            in
+            let
+              b = 1;
+            in
+            x
         "},
     ],
 }
