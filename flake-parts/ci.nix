b4cebe590f6bb9a81d01e27e4c3ac2eb2b0fe5b8
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-06-17 15:55:22 +0700

    ci: single job

--- flake-parts/ci.nix
+++ flake-parts/ci.nix
@@ -1,56 +1,11 @@
-{ lib, ... }:
 let
-  filePaths = {
-    check = ".github/workflows/check.yaml";
-  };
-
-  ids = {
-    jobs = {
-      getCheckNames = "get-check-names";
-      check = "check";
-    };
-    steps = {
-      getCheckNames = "get-check-names";
-      appToken = "app-token";
-    };
-    outputs = {
-      jobs.getCheckNames = "checks";
-      steps.getCheckNames = "checks";
-    };
-  };
-
-  matrixParam = "checks";
-
-  nixArgs = "--accept-flake-config";
-
-  runner = {
-    name = "ubuntu-latest";
-    system = "x86_64-linux";
-  };
-
-  steps = {
-    checkout.uses = "actions/checkout@v4";
-    installNix = {
-      uses = "nixbuild/nix-quick-install-action@master";
-      "with" = {
-        nix_conf = ''
-          keep-env-derivations = true
-          keep-outputs = true
-        '';
-        github_access_token = "\${{ secrets.GITHUB_TOKEN }}";
-      };
-    };
-    cacheNix = {
-      uses = "nix-community/cache-nix-action@main";
-      "with".primary-key = "nix-\${{ runner.os }}";
-    };
-  };
+  path = ".github/workflows/check.yaml";
 in
 {
   perSystem =
     { pkgs, ... }:
     {
-      files.file.${filePaths.check}.source = pkgs.writers.writeJSON "gh-actions-workflow-check.yaml" {
+      files.file.${path}.source = pkgs.writers.writeJSON "gh-actions-workflow-check.yaml" {
         name = "Check";
         on = {
           pull_request = { };
@@ -58,43 +13,32 @@ in
           workflow_dispatch = { };
         };
         jobs = {
-          ${ids.jobs.getCheckNames} = {
-            runs-on = runner.name;
-            outputs.${ids.outputs.jobs.getCheckNames} =
-              "\${{ steps.${ids.steps.getCheckNames}.outputs.${ids.outputs.steps.getCheckNames} }}";
+          check = {
+            runs-on = "ubuntu-latest";
             steps = [
-              steps.checkout
-              steps.installNix
-              steps.cacheNix
+              { uses = "actions/checkout@v4"; }
               {
-                id = ids.steps.getCheckNames;
-                run = ''
-                  checks="$(nix ${nixArgs} eval --json .#checks.${runner.system} --apply builtins.attrNames)"
-                  echo "${ids.outputs.steps.getCheckNames}=$checks" >> $GITHUB_OUTPUT
-                '';
+                uses = "nixbuild/nix-quick-install-action@master";
+                "with" = {
+                  nix_conf = ''
+                    keep-env-derivations = true
+                    keep-outputs = true
+                  '';
+                  github_access_token = "\${{ secrets.GITHUB_TOKEN }}";
+                };
               }
-            ];
-          };
-
-          ${ids.jobs.check} = {
-            needs = ids.jobs.getCheckNames;
-            runs-on = runner.name;
-            strategy.matrix.${matrixParam} =
-              "\${{ fromJson(needs.${ids.jobs.getCheckNames}.outputs.${ids.outputs.jobs.getCheckNames}) }}";
-            steps = [
-              steps.checkout
-              steps.installNix
-              steps.cacheNix
               {
-                run = ''
-                  nix ${nixArgs} build '.#checks.${runner.system}."''${{ matrix.${matrixParam} }}"'
-                '';
+                uses = "nix-community/cache-nix-action@main";
+                "with".primary-key = "nix-\${{ runner.os }}";
+              }
+              {
+                run = "nix --accept-flake-config flake check --print-build-logs";
               }
             ];
           };
         };
       };
 
-      treefmt.settings.global.excludes = lib.attrValues filePaths;
+      treefmt.settings.global.excludes = [ path ];
     };
 }
