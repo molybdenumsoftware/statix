2a61ba1ca6ad5a5db02ace8b3e5934c66036261b
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-06-17 16:20:43 +0700

    ci: no nix caching

--- flake-parts/ci.nix
+++ flake-parts/ci.nix
@@ -27,10 +27,12 @@ in
                   github_access_token = "\${{ secrets.GITHUB_TOKEN }}";
                 };
               }
-              {
-                uses = "nix-community/cache-nix-action@main";
-                "with".primary-key = "nix-\${{ runner.os }}";
-              }
+              # error: opening lock file "/nix/var/nix/temproots/2926": Permission denied
+              # https://github.com/molybdenumsoftware/statix/actions/runs/27678513502/job/81859681633?pr=2671#step:5:9
+              # {
+              #   uses = "nix-community/cache-nix-action@v7";
+              #   "with".primary-key = "nix-\${{ runner.os }}";
+              # }
               {
                 run = "nix --accept-flake-config flake check --print-build-logs";
               }
