0cad23a436114f9adc7461e41d6de34784e6ffc7
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-09-01 16:45:41 +0700

    ci: automerge dependabot

--- flake-parts/dependabot.nix
+++ flake-parts/dependabot.nix
@@ -1,30 +1,70 @@
 let
-  filePath = ".github/dependabot.yml";
+  configPath = ".github/dependabot.yml";
+  automergeWorkflowPath = ".github/workflows/dependabot-automerge.yml";
 in
 {
   perSystem =
     { pkgs, ... }:
     {
-      files.file.${filePath}.source = pkgs.writers.writeJSON "dependabot.yml" {
-        version = 2;
-        updates = [
-          {
-            package-ecosystem = "cargo";
-            directory = "/";
-            schedule.interval = "daily";
-            commit-message = {
-              prefix = "chore";
-              include = "scope";
-            };
-          }
-          {
-            package-ecosystem = "nix";
-            directory = "/";
-            schedule.interval = "daily";
-            commit-message.prefix = "chore";
-          }
-        ];
+      files.file = {
+        ${configPath}.source = pkgs.writers.writeJSON "dependabot.yml" {
+          version = 2;
+          updates = [
+            {
+              package-ecosystem = "cargo";
+              directory = "/";
+              schedule.interval = "daily";
+              commit-message = {
+                prefix = "chore";
+                include = "scope";
+              };
+            }
+            {
+              package-ecosystem = "nix";
+              directory = "/";
+              schedule.interval = "daily";
+              commit-message.prefix = "chore";
+            }
+          ];
+        };
+        ${automergeWorkflowPath}.source = pkgs.writers.writeJSON "dependabot-automerge.yml" {
+          name = "Dependabot auto-merge";
+          on = "pull_request";
+
+          permissions = {
+            contents = "write";
+            pull-requests = "write";
+          };
+
+          jobs.dependabot = {
+            runs-on = "ubuntu-latest";
+            "if" =
+              "github.event.pull_request.user.login == 'dependabot[bot]' && github.repository == 'molybdenumsoftware/statix'";
+            steps = [
+              {
+                name = "Dependabot metadata";
+                id = "metadata";
+                uses = "dependabot/fetch-metadata@main";
+                "with".github-token = "\${{ secrets.GITHUB_TOKEN }}";
+
+              }
+              {
+
+                name = "Enable auto-merge for Dependabot PRs";
+                run = ''gh pr merge --auto --merge "$PR_URL"'';
+                env = {
+
+                  PR_URL = "\${{github.event.pull_request.html_url}}";
+                  GH_TOKEN = "\${{secrets.GITHUB_TOKEN}}";
+                };
+              }
+            ];
+          };
+        };
       };
-      treefmt.settings.global.excludes = [ filePath ];
+      treefmt.settings.global.excludes = [
+        configPath
+        automergeWorkflowPath
+      ];
     };
 }
